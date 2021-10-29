9fb73d92040ba3dcfa0b6078bb8897890970e6e9
Amjed Agabani <amjedagabani@gmail.com>
2021-10-29 20:03:20 +0100

    wip: reworking webserver and auth code

--- pkg/oidc/get_authorization_code.go
+++ pkg/oidc/get_authorization_code.go
@@ -9,15 +9,28 @@ import (
 func handleCallback(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("You can now close this tab."))
 	code := r.URL.Query().Get("code")
-	fmt.Print(code)
+	fmt.Println(code)
 }
 
 func GetAuthorizationCode() string {
+	channel := make(chan string, 1)
+
+	// channel <- "SOMEHOW CODE GOES THROUGH HERE"
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleCallback)
-	err := http.ListenAndServe(":30423", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	go func() {
+		server := &http.Server{Addr: ":30423", Handler: mux}
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	authorizationCode := <-channel
+
+	fmt.Println(authorizationCode)
+
 	return ""
 }
